05ad7b1a8f4f72ebb228ee13dd23b8c8bd312a81
Carlos <carc169@gmail.com>
2015-04-22 06:54:01 -0500

    Movies and their trailers

---
+++ entertainment_center.py
@@ -0,0 +1,115 @@
+import fresh_tomatoes
+import media
+
+
+days_later = media.Movie("28 Days Later",
+                         "Humanity is faced with an apocalypse after the release of the Rage virus",
+                         "http://www.movierulz.com/wp-content/uploads/2013/07/28-Days-Later.jpg",
+                         "https://www.youtube.com/watch?v=c7ynwAgQlDQ")
+
+eight_mile = media.Movie("8 mile",
+                         "An up and coming rapper tries to make a name for himself",
+                         "http://upload.wikimedia.org/wikipedia/en/8/8b/Eight_mile_ver2.jpg",
+                         "https://www.youtube.com/watch?v=axGVrfwm9L4")
+
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet",
+                     "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX640_SY720_.jpg",
+                     "https://www.youtube.com/watch?v=_Tkc5pQp_JE")
+
+braveheart = media.Movie("Braveheart",
+                         "A man that fights for freedom even if it costs him his life",
+                         "http://upload.wikimedia.org/wikipedia/en/5/55/Braveheart_imp.jpg",
+                         "https://www.youtube.com/watch?v=wj0I8xVTV18")
+
+django = media.Movie("Django Unchained",
+                     "A slave becomes a hitman in this Quentin Tarantino thriller",
+                     "http://ia.media-imdb.com/images/M/MV5BMjIyNTQ5NjQ1OV5BMl5BanBnXkFtZTcwODg1MDU4OA@@._V1_SX214_AL_.jpg",
+                     "https://www.youtube.com/watch?v=eUdM9vrCbow")
+
+harry = media.Movie("Harry Potter ",
+                    "A young boy goes to wizard school",
+                    "http://img4.wikia.nocookie.net/__cb20150209174452/harrypotter/images/8/8d/Harry_potter_and_the_sorcerer's_stone_poster.jpg",
+                    "https://www.youtube.com/watch?v=PbdM1db3JbY")
+
+gladiator = media.Movie("Gladiator",
+                        "A general becomes a slave gladiator that challenges a tyrant king",
+                        "http://ia.media-imdb.com/images/M/MV5BMTgwMzQzNTQ1Ml5BMl5BanBnXkFtZTgwMDY2NTYxMTE@._V1_SY317_CR0,0,214,317_AL_.jpg",
+                        "https://www.youtube.com/watch?v=0BLZbrLogTo")
+
+inglourious_basterds = media.Movie("Inglourious Basterds",
+                                   "A fictional depiction of the Holocaust era",
+                                   "http://www.gstatic.com/tv/thumb/movieposters/193875/p193875_p_v7_aa.jpg",
+                                   "https://www.youtube.com/watch?v=KnrRy6kSFF0")
+
+kill_bill = media.Movie("Kill Bill",
+                        "A woman takes revenge on those that killed her husband and unborn daughter",
+                        "http://ecx.images-amazon.com/images/I/51MXGK8EZSL.jpg",
+                        "https://www.youtube.com/watch?v=ot6C1ZKyiME")
+
+pirates = media.Movie("Pirates of the Caribbean",
+                      "A pirate and his adventures",
+                      "http://cdnvideo.dolimg.com/cdn_assets/49ceb41768ec1dac5bc3c8c14ca4c53eb848c4d9.jpg",
+                      "https://www.youtube.com/watch?v=naQr0uTrH_s")
+
+the_avengers = media.Movie("The Avengers",
+                           "Superheroes avenge humanity",
+                           "http://ia.media-imdb.com/images/M/MV5BMTk2NTI1MTU4N15BMl5BanBnXkFtZTcwODg0OTY0Nw@@._V1_SX640_SY720_.jpg",
+                           "https://www.youtube.com/watch?v=eOrNdBpGMv8")
+
+bourne = media.Movie("The Bourne Identity",
+          "A man with amnesia tries to find out who he is and why people are after him",
+          "http://ia.media-imdb.com/images/M/MV5BMTQ3MDA4MDIyN15BMl5BanBnXkFtZTYwOTg0Njk4._V1_SX214_AL_.jpg",
+          "https://www.youtube.com/watch?v=cD-uQreIwEk")
+
+the_dark_knight = media.Movie("The Dark Knight",
+                              "Batman vs Joker and Two-Face",
+                              "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX640_SY720_.jpg",
+                              "https://www.youtube.com/watch?v=yQ5U8suTUw0")
+
+the_departed = media.Movie("The Departed",
+                           "A good guy that wants to be a cop has to earn it",
+                           "http://www.tamilaruvy.com/wp-content/uploads/2014/06/the-departed.jpg",
+                           "https://www.youtube.com/watch?v=SGWvwjZ0eDc")
+
+the_hunger_games = media.Movie("The Hunger Games",
+                               "Nobody is that hungry to join this competition",
+                               "http://ia.media-imdb.com/images/M/MV5BMjA4NDg3NzYxMF5BMl5BanBnXkFtZTcwNTgyNzkyNw@@._V1_SX640_SY720_.jpg",
+                               "https://www.youtube.com/watch?v=eO0T9A3kdqc")
+
+
+this_is_the_end = media.Movie("This is the End",
+                              "A hilarious ending of the world",
+                              "http://ia.media-imdb.com/images/M/MV5BMTQxODE3NjM1Ml5BMl5BanBnXkFtZTcwMzkzNjc4OA@@._V1_SX640_SY720_.jpg",
+                              "https://www.youtube.com/watch?v=Yma-g4gTwlE")
+
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "http://cdnvideo.dolimg.com/cdn_assets/5670999ffe25e4bd664bc9486adef5171a494e7f.jpg",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+underworld = media.Movie("Underworld",
+                         "Vampires vs werewolves",
+                         "http://ia.media-imdb.com/images/M/MV5BMjIxNDExNDEyMV5BMl5BanBnXkFtZTcwODY1OTkxMw@@._V1_SY317_CR0,0,214,317_AL_.jpg",
+                         "https://www.youtube.com/watch?v=RuoIQySqmMQ")
+
+movies = [days_later,
+          eight_mile,
+          avatar,
+          braveheart,
+          django,
+          gladiator,
+          harry,
+          inglourious_basterds,
+          kill_bill,
+          pirates,
+          the_avengers,
+          bourne,
+          the_dark_knight,
+          the_departed,
+          the_hunger_games,
+          this_is_the_end,
+          toy_story,
+          underworld]
+
+fresh_tomatoes.open_movies_page(movies)
